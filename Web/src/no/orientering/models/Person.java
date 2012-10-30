e041a05a37513c22eebc55195cdcd7b5277b42dc
Lorentz <lrvedeler@gmail.com>
2012-10-30 11:12:25 +0100

    La til getters & setters i Person-klassen

--- Web/src/no/orientering/models/Person.java
+++ Web/src/no/orientering/models/Person.java
@@ -1,7 +1,31 @@
 package no.orientering.models;
 
 public class Person {
-	String name;
-	String phone;
-	int birthYear;
+	private String name;
+	private String phone;
+	private int birthYear;
+	
+	public boolean isNull()
+	{
+		return false;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public int getBirthYear() {
+		return birthYear;
+	}
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
 }
