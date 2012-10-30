c41a6e1f6baf69ea1e3bd0a2ef0076489860d4ce
Lorentz <lrvedeler@gmail.com>
2012-10-30 11:12:43 +0100

    La til getters & setters i User-klassen

--- Web/src/no/orientering/models/User.java
+++ Web/src/no/orientering/models/User.java
@@ -6,6 +6,46 @@ public class User {
 	Organization team;
 	
 	Person personalia;
-	Person friend;
-	Person emergencyContact;
+	Person friend = new NullPerson();
+	Person emergencyContact = new NullPerson();
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Organization getTeam() {
+		return team;
+	}
+	public void setTeam(Organization team) {
+		this.team = team;
+	}
+	public Person getPersonalia() {
+		return personalia;
+	}
+	public void setPersonalia(Person personalia) {
+		this.personalia = personalia;
+	}
+	public Person getFriend() {
+		return friend;
+	}
+	public void setFriend(Person friend) {
+		this.friend = friend;
+	}
+	public Person getEmergencyContact() {
+		return emergencyContact;
+	}
+	public void setEmergencyContact(Person emergencyContact) {
+		this.emergencyContact = emergencyContact;
+	}
+	public int getUserId() {
+		return userId;
+	}
 }
