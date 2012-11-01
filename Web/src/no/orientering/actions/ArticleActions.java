e22bac562171b7b1aec989394e2e6f66ca93936d
Lorentz <lrvedeler@gmail.com>
2012-11-01 15:03:36 +0100

    RestActions

---
+++ Web/src/no/orientering/actions/ArticleActions.java
@@ -0,0 +1,21 @@
+package no.orientering.actions;
+
+import java.util.List;
+
+import no.orientering.DAO.jdbc.PersonDAO;
+import no.orientering.models.*;
+
+public class ArticleActions {
+	public void postArticle(){
+		
+	}
+	public void putArticle(){
+		
+	}
+	public List<Article> getArticle(){
+		
+	}
+	public Article getArticle(int id){
+		
+	}
+}
