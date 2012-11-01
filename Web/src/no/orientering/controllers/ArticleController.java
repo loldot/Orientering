1a4df329a1fb09179acef18d582a30c9199e4982
Markus Skallist <markusrs@online.no>
2012-11-01 14:28:02 +0100

    r

--- Web/src/no/orientering/controllers/ArticleController.java
+++ Web/src/no/orientering/controllers/ArticleController.java
@@ -20,45 +20,48 @@ import no.orientering.utils.NetHelp;
 @WebServlet("/ArticleController")
 public class ArticleController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ArticleController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ArticleController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String articleID = request.getParameter("articleID");
 		ArticleDAO adao = new ArticleDAO();
-		if (!NetHelp.isNullOrEmpty(articleID)){
+		if (!NetHelp.isNullOrEmpty(articleID)) {
 			int id = Integer.parseInt(articleID);
-			if (id > 0){
-				
+			if (id > 0) {
+
 				Article art = adao.getArticle(id);
-				request.setAttribute("article",art);
+				request.setAttribute("article", art);
 				// fetch 1
-			}else {
-				//liste
-				List<Article> artList = adao.getArticles();
-				request.setAttribute("articles",artList);
 			}
-			
-			
+
+		} else {
+			List<Article> artList = adao.getArticles();
+			request.setAttribute("articles", artList);
 		}
-		RequestDispatcher dispatcher =	request.getRequestDispatcher("WEB-INF/articles.jsp");
+		RequestDispatcher dispatcher = request
+				.getRequestDispatcher("WEB-INF/articles.jsp");
 		dispatcher.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
