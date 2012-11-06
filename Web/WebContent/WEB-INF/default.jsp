60206a8a983eb203b03b6b9a44fc34b3b9090021
Markus Skallist <markusrs@online.no>
2012-11-06 21:11:28 +0100

    artikler

--- Web/WebContent/WEB-INF/default.jsp
+++ Web/WebContent/WEB-INF/default.jsp
@@ -1,6 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
 <jsp:include page="header.jsp" />
 <title>Home | O-løp er OK</title>
 
@@ -8,7 +9,30 @@
 </head>
 <body>
 	<jsp:include page="top.jsp"></jsp:include>
-	<div id="content"></div>
+	<div id="content">
+
+		<c:forEach var="article" items="${articles}">
+
+			<div class="article">
+				<div class="article_title">
+					<h3>${article.title}</h3>
+				</div>
+				<div class="article_head">
+				<fmt:formatDate type="BOTH" dateStyle="long" timeStyle="long"
+						value="${article.publishedDate}" /> | Skrevet av 
+					${article.author.personalia.firstName}
+					${article.author.personalia.lastName} 
+					
+				</div>
+				<div class="article_content">${article.content}</div>
+				<div class="article_footer"></div>
+			</div>
+
+		</c:forEach>
+
+
+
+	</div>
 
 	<jsp:include page="bottom.jsp"></jsp:include>
 </body>
--- Web/src/no/orientering/controllers/HomeController.java
+++ Web/src/no/orientering/controllers/HomeController.java
@@ -1,11 +1,17 @@
 package no.orientering.controllers;
 
 import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import no.orientering.DAO.jdbc.ArticleDAO;
+import no.orientering.models.Article;
 
 /**
  * Servlet implementation class HomeController
@@ -13,26 +19,39 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/HomeController")
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public HomeController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public HomeController() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String URL = "WEB-INF/default.jsp";
+
+		ArticleDAO ad = new ArticleDAO();
+		List<Article> artlist = ad.getArticles();
+		
+		request.setAttribute("articles", artlist);
+		RequestDispatcher dispatcher = request.getRequestDispatcher(URL);
+		dispatcher.forward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 	}
 
