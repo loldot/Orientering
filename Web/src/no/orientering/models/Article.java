a9f90b9227e92c26c3a9301979ac595cb80b2531
Lorentz <lrvedeler@gmail.com>
2012-11-08 14:57:28 +0100

    article har default dagens dato

--- Web/src/no/orientering/models/Article.java
+++ Web/src/no/orientering/models/Article.java
@@ -1,6 +1,7 @@
 package no.orientering.models;
 
 import java.net.URI;
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -26,6 +27,7 @@ public class Article {
 	
 	public Article(){
 		ID = 0;
+		publishedDate = Calendar.getInstance().getTime();
 	}
 	public Article(int ID){
 		this.ID = ID;
